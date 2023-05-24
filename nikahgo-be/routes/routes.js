8d715fbf29fc25041130718654a954dcc7ed21dc
badzaini <badzaini28@gmail.com>
2023-05-24 09:29:49 +0800

    first push

---
+++ nikahgo-be/routes/routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const Model = require('../models/model');
+
+const router = express.Router()
+
+module.exports = router;
+
+//Post Method
+// router.post('/post', (req, res) => {
+//     res.send('Post API')
+// })
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
+//Get all Method
+router.get('/getAll', async (req, res) => {
+    const user = await Model.find();
+
+    res.status(200).json({ user });
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send(req.params.id)
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
