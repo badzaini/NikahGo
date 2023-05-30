386f90c0fff0bc7bfab1a86a8e6970c83930f572
lqmnwido <luqmanhafiz247@gmail.com>
2023-05-30 16:43:31 +0800

    update

---
+++ view/pages/module4/consultationApplicationView/ConsultationForm/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ConsultationForm from '../../../../screens/module4/consultationApplicationView/ConsultationForm';
+
+export default function ConsultationFormPage() {
+  return (
+    <ConsultationForm />
+  );
+}
