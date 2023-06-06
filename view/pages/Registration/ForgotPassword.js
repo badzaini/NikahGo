26466e2f628cf89ab501f84c9bef33ce0dbe4890
AmanulHakim123 <amanul_hakim@yahoo.com>
2023-06-06 15:22:18 +0800

    fix code

--- view/pages/Registration/ForgotPassword.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ForgotPassword from '../../screens/module1/forgotPassword';
-
-export default function ForgotPasswordPage() {
-  return (
-    <ForgotPassword />
-  );
-}
